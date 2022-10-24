6d85fa428d9a73f6f32747df43e95d5d6a283af8
Woyken <13166802+Woyken@users.noreply.github.com>
2022-10-25 02:36:35 +0300

    don't use local certificates when deployed

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,14 +8,16 @@ import { createServer } from "https";
 import fs from "fs";
 import { AxiosError } from "axios";
 import { RegisterRoutes } from "../build/routes";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 const options = {
   key: fs.readFileSync(__dirname + "/ssl/key.pem"),
   cert: fs.readFileSync(__dirname + "/ssl/cert.pem"),
 };
-const server = createServer(options, app);
+const server = process.env.NODE_ENV === 'development'
+  ? createServer(options, app)
+  : createServer(app);
 const port = process.env.PORT || 3000;
 
 var allowedOrigins = ["https://localhost:3001", "https://woyken.github.io"];
