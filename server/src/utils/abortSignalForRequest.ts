200e3aa0ba3a1e0f657f05163bf186669263b94e
Woyken <13166802+Woyken@users.noreply.github.com>
2022-11-04 10:11:44 +0200

    call abort on req close

--- server/src/utils/abortSignalForRequest.ts
+++ server/src/utils/abortSignalForRequest.ts
@@ -4,11 +4,11 @@ export function getAbortSignalForRequest(req: Request) {
   const abortController = new AbortController();
   req.once("close", () => {
     console.log("request close event received ", req.url);
+    abortController.abort();
+    console.log("abort() called ", req.url);
   });
   req.once("error", () => {
     console.log("request error event received ", req.url);
-    abortController.abort();
-    console.log("abort() called ", req.url);
   });
   return abortController.signal;
 }
