12c524012f9f6f0fb782abfed8a0a77f9342b844
Woyken <13166802+Woyken@users.noreply.github.com>
2022-11-04 09:31:15 +0200

    Added some more logging for abort signal

--- server/src/utils/abortSignalForRequest.ts
+++ server/src/utils/abortSignalForRequest.ts
@@ -2,6 +2,13 @@ import { Request } from "express";
 
 export function getAbortSignalForRequest(req: Request) {
   const abortController = new AbortController();
-  req.on("close", () => abortController.abort());
+  req.once("close", () => {
+    console.log("request close event received ", req.url);
+    abortController.abort();
+    console.log("abort() called ", req.url);
+  });
+  req.once("error", () => {
+    console.log("request error event received ", req.url);
+  });
   return abortController.signal;
 }
