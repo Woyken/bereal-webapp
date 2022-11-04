e0735ec913d0859844909e0b721b8fe4bde75323
Woyken <13166802+Woyken@users.noreply.github.com>
2022-11-04 10:15:31 +0200

    request cancelling works, no need for logging

--- server/src/utils/abortSignalForRequest.ts
+++ server/src/utils/abortSignalForRequest.ts
@@ -2,13 +2,6 @@ import { Request } from "express";
 
 export function getAbortSignalForRequest(req: Request) {
   const abortController = new AbortController();
-  req.once("close", () => {
-    console.log("request close event received ", req.url);
-    abortController.abort();
-    console.log("abort() called ", req.url);
-  });
-  req.once("error", () => {
-    console.log("request error event received ", req.url);
-  });
+  req.once("close", () => abortController.abort());
   return abortController.signal;
 }
