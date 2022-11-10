4edc2d57cd0b4c3c2519d093c78b808ac9bc5c77
Woyken <13166802+Woyken@users.noreply.github.com>
2022-11-10 21:59:34 +0200

    rewrite image to use css in js

--- client/src/components/berealFeedImage.tsx
+++ client/src/components/berealFeedImage.tsx
@@ -1,19 +1,43 @@
-import clsx from "clsx";
+import { Box, Image, css, classNames } from "@hope-ui/solid";
 import { createMemo, createSignal } from "solid-js";
-import styles from "./berealFeedImage.module.css";
 
 type Props = {
   primaryUrl: string;
   secondaryUrl: string;
 };
 
+const feedImageStyles = css({
+  "max-width": "40em",
+  "transition-property": "all",
+  "transition-duration": "0.2s",
+  "transition-delay": "0s",
+  "transition-timing-function": "cubic-bezier(.18,.89,.32,1.28)",
+});
+
+const topImageStyles = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  "z-index": 1,
+  transform: "scale(0.3)",
+  "transform-origin": "0 0",
+  margin: "3%",
+  width: "100%",
+  borderRadius: "$3xl"
+});
+
+const bottomImageStyles = css({
+  width: "100%",
+  borderRadius: "$lg",
+});
+
 const BerealFeedImage = (props: Props) => {
   const [isPrimaryOnTop, setIsPrimaryOnTop] = createSignal(false);
-  const primaryStyle = createMemo(
-    () => styles[isPrimaryOnTop() ? "top-image" : "bottom-image"]
+  const primaryStyle = createMemo(() =>
+    isPrimaryOnTop() ? topImageStyles() : bottomImageStyles()
   );
-  const secondaryStyle = createMemo(
-    () => styles[isPrimaryOnTop() ? "bottom-image" : "top-image"]
+  const secondaryStyle = createMemo(() =>
+    isPrimaryOnTop() ? bottomImageStyles() : topImageStyles()
   );
 
   const handlePrimaryClick = () => {
@@ -24,20 +48,21 @@ const BerealFeedImage = (props: Props) => {
   };
 
   return (
-    <div class={styles["relative-wrapper"]}>
-      <img
-        class={clsx(styles["feed-image"], primaryStyle())}
+    <Box position="relative">
+      <Image
+        borderRadius="$lg"
+        class={classNames(feedImageStyles(), primaryStyle())}
         src={props.primaryUrl}
         loading="lazy"
         onclick={handlePrimaryClick}
       />
-      <img
-        class={clsx(styles["feed-image"], secondaryStyle())}
+      <Image
+        class={classNames(feedImageStyles(), secondaryStyle())}
         src={props.secondaryUrl}
         loading="lazy"
         onclick={handleSecondaryClick}
       />
-    </div>
+    </Box>
   );
 };
 
